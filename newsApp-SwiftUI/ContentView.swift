d25772c678b94044cf55eed9b227cdfe974e8f22
mazen moataz <85955591+mazenmo0taz@users.noreply.github.com>
2021-10-10 06:53:29 +0200

    Initial Commit

---
+++ newsApp-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newsApp-SwiftUI
+//
+//  Created by mazen moataz on 10/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ newsApp-SwiftUI/newsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp_SwiftUIApp.swift
+//  newsApp-SwiftUI
+//
+//  Created by mazen moataz on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct newsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
